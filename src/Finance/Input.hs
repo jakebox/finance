fe6b226740c5bc3bb053c11ee895002978e902b5
Jake B <jacobboxerman@gmail.com>
2025-07-01 21:25:11 -0700

    Month parser

--- src/Finance/Input.hs
+++ src/Finance/Input.hs
@@ -4,15 +4,19 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module Finance.Input (readTransactionFile) where
+module Finance.Input (readTransactionFile, parseMonth) where
 
 import qualified Data.ByteString.Lazy as BS
 import Data.Csv
 import Data.Decimal
-import Data.Time
+import Data.Time (Day)
+import Data.Time.Calendar (MonthOfYear)
 import qualified Data.Vector as V
 import Finance.Types
 import Finance.Utils
+
+import Text.Parsec
+import qualified Text.Parsec.String as P (Parser)
 
 instance FromField Day where
   parseField :: Field -> Parser Day
@@ -48,3 +52,26 @@ readTransactionFile fp = do
   case decodeByName csvData of
     Left err -> error err
     Right (_, txs :: V.Vector Transaction) -> pure $ V.toList txs
+
+
+-- --- Parsers
+parseMonth :: P.Parser MonthOfYear
+parseMonth = do
+  monthStr <- many1 anyChar
+  pure $ arbitraryMonToMonth monthStr
+
+arbitraryMonToMonth :: String -> MonthOfYear
+arbitraryMonToMonth s 
+  | s == "jan" || s == "january" || s == "01" || s == "1" = 1
+  | s == "feb" || s == "february" || s == "02" || s == "2" = 2
+  | s == "mar" || s == "march" || s == "03" || s == "3" = 3
+  | s == "apr" || s == "april" || s == "04" || s == "4" = 4
+  | s == "may" || s == "05" || s == "5" = 5
+  | s == "jun" || s == "june" || s == "06" || s == "6" = 6
+  | s == "jul" || s == "july" || s == "07" || s == "7" = 7
+  | s == "aug" || s == "august" || s == "08" || s == "8" = 8
+  | s == "sep" || s == "september" || s == "09" || s == "9" = 9
+  | s == "oct" || s == "october" || s == "10" || s == "10" = 10
+  | s == "nov" || s == "november" || s == "11" || s == "11" = 11
+  | s == "dec" || s == "december" || s == "12" || s == "12" = 12
+  | otherwise = error $ "Invalid month string: " ++ s
